6ccee73433dd8498d23a18e1ccc804b56013ff51
Shubhankar <shuboyjee1@gmail.com>
2025-08-22 20:58:00 +0530

    refactor:deleted-separate-be

--- geminiProject/backend/src/utils/groqClient.ts
+++
@@ -1,6 +0,0 @@
-import Groq from "groq-sdk";
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({ path: path.resolve("src/config/.env") });
-export const groq = new Groq({ apiKey: process.env.GROQ_API_KEY_LLAMA3_8B });
